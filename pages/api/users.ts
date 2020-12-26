6340ba4c8cad7f32df3b28ae91e41d43a584ff2a
kyle <kyle@solidstategroup.com>
2020-12-26 17:44:34 +0000

    test

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,17 +1,7 @@
-// Import Dependencies
-import url from 'url'
 import {getCollection} from '../../server/connect'
-// Create cached connection variable
 
-
-// The main, exported, function of the endpoint,
-// dealing with the request and subsequent response
 export default async (req, res) => {
-
     const collection = await getCollection("users")
-    // Select the users collection from the database
     const users = await collection.find({}).toArray()
-
-    // Respond with a JSON string of all users in the collection
     res.status(200).json({ users })
 }
