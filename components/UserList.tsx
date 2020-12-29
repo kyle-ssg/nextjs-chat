a0f1aaea6ba6ad9206c591a92c1bc07fc127c8b7
kyle <kyle@solidstategroup.com>
2020-12-29 19:37:54 +0000

    Send private message

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -5,27 +5,53 @@ import {DEFAULT_AVATAR} from "../common/constants"; // we need this to make JSX 
 import cx from "classname";
 import safeEventParse from "../common/saveEventParse";
 import sendHeartbeat from "../common/sendHeartbeat";
+import { ContextMenu, ContextMenuTrigger, MenuItem as ContextMenuItem } from "react-contextmenu";
+import Link from 'next/link';
+import MessageIcon from "./icons/MessageIcon";
+import {useRouter} from "next/router";
+import useAuth from "../common/useAuth";
+
 type ComponentType = {}
 
 type UserSummaryType = {
     user:IUserBase,
+    onClick:(user:IUserBase)=>void,
     active?:boolean
 }
 
-const UserSummary: FunctionComponent<UserSummaryType> = ({user, active}) => {
+type MenuItemIype = {
+    text:string,
+    icon:React.ReactNode
+}
+
+const MenuItem: FunctionComponent<MenuItemIype> = ({icon,text}) => {
     return (
-        <div className={cx("flex-row user-summary mb-2",{active,admin:user.role==="ADMIN"})}>
+        <div className="menu-item flex-row">
+            {icon}
+            {text}
+        </div>
+    )
+}
+
+
+const UserSummary: FunctionComponent<UserSummaryType> = ({user, onClick, active}) => {
+    return (
+        <div onClick={()=>onClick(user)} className={cx("flex-row user-summary mb-2",{active,admin:user.role==="ADMIN"})}>
             <img className="avatar" src={user.avatar||DEFAULT_AVATAR}/>
             <div className="username ml-2">
                 {user.username}
             </div>
+
         </div>
     )
 }
 let interval;
 const UserList: FunctionComponent<ComponentType> = ({}) => {
+    const {user: currentUser} = useAuth();
     const {getUsers, inactiveUsers, activeUsers} = useUserList();
     const [filter, setFilter] = useState<string>("")
+    const [activeUser, setActiveUser] = useState<IUserBase>(null);
+    const router = useRouter()
     useEffect(()=>{
         if (!interval) {
             interval = setInterval(()=>{
@@ -45,21 +71,56 @@ const UserList: FunctionComponent<ComponentType> = ({}) => {
        return  user.username.toLowerCase().includes(search)
     }) : inactiveUsers
 
+    function handleClick(e, data) {
+        switch (data.type) {
+            case "SEND":{
+                router.push("/?room=private-"+activeUser._id)
+                break;
+            }
+        }
+    }
     return (
         <>
             <div className="input-container-default input-container-sm mx-2 mt-2">
                 <input onChange={(e)=>setFilter(safeEventParse(e))} placeholder="Search..." type="text"/>
             </div>
+
+            <ContextMenu style={{width:200, height:100}} id="same_unique_identifier">
+                <div>
+                    <div className="text-center">
+                        <h4>{activeUser?.username}</h4>
+                    </div>
+                    <ContextMenuItem onClick={handleClick} data={{type:"SEND"}}>
+                            <MenuItem icon={<MessageIcon/>} text="Send Message" />
+                    </ContextMenuItem>
+                </div>
+            </ContextMenu>
             <div className="ml-2 user-list mt-2">
                 {
-                    filteredActiveUsers?.map((user)=>(
-                        <UserSummary active key={user._id} user={user}/>
-                    ))
+                    filteredActiveUsers?.map((user)=> {
+                        const summary = <UserSummary onClick={setActiveUser} active key={user._id} user={user}/>;
+                        if (!currentUser?._id || (user._id === currentUser?._id)) {
+                            return summary
+                        }
+                        return (
+                            <ContextMenuTrigger key={user._id} mouseButton={0} id="same_unique_identifier">
+                                {summary}
+                            </ContextMenuTrigger>
+                        )
+                    })
                 }
                 {
-                    filteredInactiveUsers?.map((user)=>(
-                        <UserSummary key={user._id} user={user}/>
-                    ))
+                    filteredInactiveUsers?.map((user)=> {
+                        const summary = <UserSummary onClick={setActiveUser} key={user._id} user={user}/>;
+                        if (!currentUser?._id || (user._id === currentUser?._id)) {
+                            return summary
+                        }
+                        return (
+                            <ContextMenuTrigger key={user._id} mouseButton={0} id="same_unique_identifier">
+                                {summary}
+                            </ContextMenuTrigger>
+                        )
+                    })
                 }
             </div>
         </>
